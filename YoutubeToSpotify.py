48b9013e50319e1c4f62e7ea5623142f56e0f24a
mfafzal <fahad.afzal780@gmail.com>
2025-11-07 21:16:22 -0700

    Done the Converstion function
    Will now download the song and put it into the proper folder

--- YoutubeToSpotify.py
+++ YoutubeToSpotify.py
@@ -1,19 +1,26 @@
 from yt_dlp import *
 from pydub import *
 from ffmpeg import *
+import shutil
 import os
 
 def Converstion():
+    '''
+    Purpose: This function will prompt you for the url, download the audio of the video, and then put it into the proper folder.
+    '''
     url = input("Please enter the url of the youtube video you want to send: ")
     name = input("Please enter the name of the song: ")
     fileName= name+".mp3"
     os.system(f'yt-dlp --extract-audio --audio-format mp3 --output "{fileName}" "{url}"')
-    
-'''
-    # Step 2: load the audio in Python
-    audio = AudioSegment.from_file("output.mp3", format="mp3")
+    cwd = os.getcwd()
+    currentPath = cwd+"\\"+fileName
+    destination = "C:\\Users\\Fahad\\Documents\\music"  #Change this to the path of the folder you have designated spotify to pull music from
+    shutil.move(currentPath, destination)
 
-    https://www.youtube.com/watch?v=Qr_QLv1TPcY
-'''
+def Email():
+    '''
+    this function is to email yourself the mp3 file so that you can download it on your phone and be able to play the song on your phone also
+    '''
+
 
 Converstion()
